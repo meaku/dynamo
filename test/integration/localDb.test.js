685cbc8deceeae479045fd264793c0a24e058952
meaku <michael.jaser@peerigon.com>
2015-02-09 18:13:04 +0100

    Add localDB integration test (wip)

---
+++ test/integration/localDb.test.js
@@ -0,0 +1,67 @@
+"use strict";
+
+var expect = require("chai").expect,
+    localDb = require("../support/localDynamoDb");
+
+var DynamoDB = require("../../lib");
+
+describe("Local DynamoDB", function () {
+
+    describe("LocalDB Helper", function () {
+
+        it("should be able to start and stop a local instance", function () {
+            return localDb
+                .start()
+                .then(function (dbProcess) {
+                    expect(dbProcess.pid).to.be.a("number");
+
+                    return localDb.stop();
+                });
+        });
+    });
+
+    describe("Basics", function () {
+
+        var db;
+
+        before(function () {
+            return localDb.start();
+        });
+
+        before(function() {
+            db = new DynamoDB({
+                apiVersion: '2014-04-24',
+                endpoint: "http://localhost:8000"
+                // accessKeyId: SET VIA ENV ,
+                // secretAccessKey: SET VIA ENV,
+                //region: SET VIA ENV
+            });
+        });
+
+        after(function () {
+            localDb.stop();
+        });
+
+        it("should be able to retrieve the tables the promise way", function () {
+            return db.listTables({}).then(function(data) {
+                expect(data.TableNames).to.eql([]);
+            });
+        });
+
+        it("should be able to retrieve the tables the async way", function (done) {
+            db.listTables({}, function(err, data) {
+                if(err) {
+                    done(err);
+                    return;
+                }
+
+                expect(data.TableNames).to.eql([]);
+                done();
+            });
+        });
+    });
+});
+
+
+
+
