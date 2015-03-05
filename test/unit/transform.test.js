6764596f67f3ec920d7876c1f5d620955c6662a0
meaku <michael.jaser@peerigon.com>
2015-03-05 13:36:55 +0100

    Update transform.test to test against new API

--- test/unit/transform.test.js
+++ test/unit/transform.test.js
@@ -1,125 +1,113 @@
 "use strict";
 
-var expect = require("chai").expect,
-    inspect = require("util").inspect;
+var expect = require("chai").expect;
 
-var transform = require("../../lib/transform"),
-    schemas = require("../support/dummies/schemas");
+var transform = require("../../lib/transform");
 
 describe("transform", function () {
 
-    var t;
+  describe("to", function () {
 
-    before(function() {
-        t = transform(schemas);
-    });
+    it("should throw if item is missing", function () {
 
-    describe("to", function () {
-
-        it("should throw if item is missing", function() {
-
-           expect(function() {
-               t.to("TestTable");
-           }).to.throwError;
-
-        });
-
-        it("should convert an object to the dynamodb represenation", function() {
-
-            var obj = {
-                UserId: 1,
-                FileId: "xy",
-                Name: "bla",
-                Size: 3,
-                ItemsOnMyDesk: ["a", "b"],
-                testBoolean: true,
-                Pens: {
-                    a: "aa",
-                    b: "bb"
-                },
-                Quantity: 12
-            };
-
-            var res = t.to("TestTable", obj);
-
-            //console.log(inspect(res, {depth: 100}));
-
-            expect(res).to.eql({
-                UserId: {S: '1'},
-                FileId: {S: 'xy'},
-                Name: {S: 'bla'},
-                Size: {N: '3'},
-                ItemsOnMyDesk: {
-                    L: [
-                        {S: 'a'},
-                        {S: 'b'}
-                    ]
-                },
-                Pens: {
-                    M: {
-                        a: {S: 'aa'},
-                        b: {S: 'bb'}
-                    }
-                },
-                testBoolean: {BOOL: true},
-                Quantity: {N: '12'}
-            });
-
-        });
+      expect(function () {
+        transform.to();
+      }).to.throwError;
 
     });
 
+    it("should convert an object to the dynamodb represenation", function () {
 
-     describe("from", function () {
+      var obj = {
+        UserId: 1,
+        FileId: "xy",
+        Name: "bla",
+        Size: 3,
+        ItemsOnMyDesk: ["a", 1],
+        testBoolean: true,
+        Pens: {
+          a: "aa",
+          b: "bb"
+        },
+        Quantity: 12
+      };
 
-         it("should throw if item is missing", function() {
+      var res = transform.to(obj);
 
-             expect(function() {
-                 t.from("TestTable");
-             }).to.throwError;
+      expect(res).to.eql({
+        UserId: {N: "1"},
+        FileId: {S: "xy"},
+        Name: {S: "bla"},
+        Size: {N: "3"},
+        ItemsOnMyDesk: {
+          L: [
+            {S: "a"},
+            {N: "1"}
+          ]
+        },
+        Pens: {
+          M: {
+            a: {S: "aa"},
+            b: {S: "bb"}
+          }
+        },
+        testBoolean: {BOOL: true},
+        Quantity: {N: "12"}
+      });
 
-         });
-
-         it("should convert a dynamodb represenation back to an object", function() {
-
-             var dynamoData = {
-                 UserId: {S: '1'},
-                 FileId: {S: 'xy'},
-                 Name: {S: 'bla'},
-                 Size: {N: '3'},
-                 ItemsOnMyDesk: {
-                     L: [
-                         {S: 'a'},
-                         {S: 'b'}
-                     ]
-                 },
-                 Pens: {
-                     M: {
-                         a: {S: 'aa'},
-                         b: {S: 'bb'}
-                     }
-                 },
-                 testBoolean: {BOOL: true},
-                 Quantity: {N: '12'}
-             };
-
-             var res = t.from("TestTable", dynamoData);
-
-             //console.log(inspect(res, {depth: 100}));
-
-             expect(res).to.eql({
-                 UserId: "1",
-                 FileId: "xy",
-                 Name: "bla",
-                 Size: 3,
-                 ItemsOnMyDesk: ["a", "b"],
-                 testBoolean: true,
-                 Pens: {
-                     a: "aa",
-                     b: "bb"
-                 },
-                 Quantity: 12
-             });
-         });
     });
+
+  });
+
+
+  describe("from", function () {
+
+    it("should throw if item is missing", function () {
+
+      expect(function () {
+        transform.from();
+      }).to.throwError;
+
+    });
+
+    it("should convert a dynamodb represenation back to an object", function () {
+
+      var dynamoData = {
+        UserId: {N: "1"},
+        FileId: {S: "xy"},
+        Name: {S: "bla"},
+        Size: {N: "3"},
+        ItemsOnMyDesk: {
+          L: [
+            {S: "a"},
+            {N: 1}
+          ]
+        },
+        Pens: {
+          M: {
+            a: {S: "aa"},
+            b: {S: "bb"}
+          }
+        },
+        testBoolean: {BOOL: true},
+        Quantity: {N: "12"}
+      };
+
+      var res = transform.from(dynamoData);
+
+      expect(res).to.eql({
+        UserId: 1,
+        FileId: "xy",
+        Name: "bla",
+        Size: 3,
+        ItemsOnMyDesk: ["a", 1],
+        testBoolean: true,
+        Pens: {
+          a: "aa",
+          b: "bb"
+        },
+        Quantity: 12
+      });
+    });
+  });
 });
